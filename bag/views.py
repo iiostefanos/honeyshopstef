8a757586cfff61363dfd362120b440d42d2a8fd6
StefanoS <iiostefanos@gmail.com>
2020-07-09 22:59:30 +0000

    added cart context and delivery logic

---
+++ bag/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def view_bag(request):
+
+
+    """ A view that renders the bag contents renders page """
+
+    return render(request, 'bag/bag.html')
