8ccf8dabc3fc08aadf1f9f4f9634f693a384c813
StefanoS <iiostefanos@gmail.com>
2020-07-29 13:10:36 +0000

    part 5A of deploy

--- honeyshopstef/settings.py
+++ honeyshopstef/settings.py
@@ -186,16 +186,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'honeyshopstef'
-    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    #'whitenoise.storage.CompressedManifestStaticFilesStorage',
-     
-
+    #'whitenoise.storage.CompressedManifestStaticFilesStorage',   
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
