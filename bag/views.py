66423b10c3212b3cc84dea3ca123e798385cf710
StefanoS <iiostefanos@gmail.com>
2020-07-30 14:48:25 +0000

    more styling and adjustments, cart refactor

--- bag/views.py
+++ bag/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
-
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
 from django.contrib import messages
 
 from products.models import Product
 # Create your views here.
 
+
 def view_bag(request):
     """ A view that renders the cart contents page """
 
     return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
@@ -28,22 +31,22 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
 
+
 def adjust_bag(request, item_id):
     
     """Adjust the quantity of the specified product to the specified amount"""
-    
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     
     if quantity > 0:
-            bag[item_id] = quantity
-            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+        bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
             
     else:
-           bag.pop(item_id)
-           messages.success(request, f'Removed {product.name} from your cart')
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cart')
     
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -62,5 +65,4 @@ def remove_from_bag(request, item_id):
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
-
-        
+    
